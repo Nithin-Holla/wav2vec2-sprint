a057bd4f053b7253ab396a6295c4594e96a839bd
nithin19 <nithin@amberscript.com>
2021-03-25 11:11:07 +0100

    Increasing eval steps

--- train_nl.sh
+++ train_nl.sh
@@ -2,7 +2,7 @@
 python run_common_voice.py \
     --model_name_or_path="facebook/wav2vec2-large-xlsr-53" \
     --dataset_config_name="nl" \
-    --output_dir=/workspace/models/nl/wav2vec2-large-xlsr-dutch-demo \
+    --output_dir=/workspace/models/nl/wav2vec2-large-xlsr-dutch \
     --overwrite_output_dir \
     --num_train_epochs="30" \
     --per_device_train_batch_size="32" \
@@ -12,13 +12,14 @@ python run_common_voice.py \
     --warmup_steps="500" \
     --fp16 \
     --freeze_feature_extractor \
-    --save_steps="400" \
-    --eval_steps="400" \
-    --save_total_limit="2" \
-    --logging_steps="100" \
+    --save_steps="1000" \
+    --eval_steps="1000" \
+    --save_total_limit="4" \
+    --logging_steps="1000" \
     --group_by_length \
     --feat_proj_dropout="0.0" \
     --layerdrop="0.1" \
     --gradient_checkpointing \
     --do_train --do_eval \
-    --preprocessing_num_workers="4"
+    --preprocessing_num_workers="4" \
+    --dataloader_num_workers="4"
