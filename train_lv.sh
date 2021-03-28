aabeadb9de42a95192f3c39f42d620ff1cdbb264
nithin19 <nithin@amberscript.com>
2021-03-28 21:51:51 +0200

    Train Latvian

---
+++ train_lv.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+python run_common_voice.py \
+    --model_name_or_path="facebook/wav2vec2-large-xlsr-53" \
+    --dataset_config_name="lv" \
+    --output_dir=/workspace/models/nl/wav2vec2-large-xlsr-latvian \
+    --overwrite_output_dir \
+    --num_train_epochs="30" \
+    --per_device_train_batch_size="32" \
+    --per_device_eval_batch_size="32" \
+    --evaluation_strategy="steps" \
+    --learning_rate="3e-4" \
+    --warmup_steps="500" \
+    --fp16 \
+    --freeze_feature_extractor \
+    --save_steps="500" \
+    --eval_steps="500" \
+    --save_total_limit="5" \
+    --logging_steps="500" \
+    --group_by_length \
+    --feat_proj_dropout="0.0" \
+    --layerdrop="0.1" \
+    --gradient_checkpointing \
+    --do_train --do_eval \
+    --preprocessing_num_workers="4" \
+    --dataloader_num_workers="4"
